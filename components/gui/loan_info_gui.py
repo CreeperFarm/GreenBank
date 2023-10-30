3a978f1376142caaa9292c5e9debc28aa92c0e18
CreeperFarm <creeperfarm.pro@gmail.com>
2023-10-30 20:04:27 +0100

    Update loan_info_gui.py

--- components/gui/loan_info_gui.py
+++ components/gui/loan_info_gui.py
@@ -12,31 +12,31 @@ def loan_info():
 
     # Define the label for the vehicule type, the entry and the position
     ttk.Label(window, text="Type de véhicule : ").grid(row=0, column=0)
-    entry_vehicule_type = ttk.Entry(window, width=30)
+    entry_vehicule_type = ttk.Entry(window, width=15)
     entry_vehicule_type.grid(row=0, column=1)
 
     # Define the label for the energy type, the entry and the position
     ttk.Label(window, text="Type d'énergie : ").grid(row=1, column=0)
-    entry_energy_type = ttk.Entry(window, width=30)
+    entry_energy_type = ttk.Entry(window, width=15)
     entry_energy_type.grid(row=1, column=1)
 
     # Define the label for the mileage, the entry and the position
     ttk.Label(window, text="Kilométrage par an : ").grid(row=2, column=0)
-    entry_mileage = ttk.Entry(window, width=30)
+    entry_mileage = ttk.Entry(window, width=15)
     entry_mileage.grid(row=2, column=1)
 
     # Define the label for the year, the entry and the position
     ttk.Label(window, text="Année : ").grid(row=3, column=0)
-    entry_year = ttk.Entry(window, width=30)
+    entry_year = ttk.Entry(window, width=15)
     entry_year.grid(row=3, column=1)
 
     # Define the label for the number of passengers, the entry and the position
     ttk.Label(window, text="Nombre de passagers : ").grid(row=4, column=0)
-    entry_passenger = ttk.Entry(window, width=30)
+    entry_passenger = ttk.Entry(window, width=15)
     entry_passenger.grid(row=4, column=1)
 
     # Define the button to calculate the eco note and the position
-    ttk.Button(window, text="Calculer l'éco note", command=window.quit).grid(row=5, column=0)
+    ttk.Button(window, text="Calculer l'éco note", command=window.quit).grid(row=5, column=0, columnspan=2)
 
     # Create the window
     window.mainloop()
